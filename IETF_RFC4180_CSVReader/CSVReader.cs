d5d619cdf70a4aa997eebb45889ea462863d14b3
许兴逸 <853974536@qq.com>
2023-12-21 17:48:40 +0800

    init

---
+++ IETF_RFC4180_CSVReader/CSVReader.cs
@@ -0,0 +1,127 @@
+﻿#nullable enable
+
+using System.Text;
+
+namespace IETF_RFC4180_CSV
+{
+    /// <summary>
+    /// CSV阅读器
+    /// 其中CSV格式遵守IETF RFC 4180中所描述的格式，同时也允许修改分隔符以读取TSV和DSV
+    /// https://www.ietf.org/rfc/rfc4180.txt
+    /// </summary>
+    public class CSVReader : IDisposable
+    {
+        /// <summary>
+        /// 解析csv文件时所使用的分隔符，修改此值以解析tsv、dsv等文件
+        /// </summary>
+        /// <value>分隔符</value>
+        public char Separator { get; set; } = ',';
+
+        /// <summary>
+        /// 从读取csv文件的TextReader构造IETF_RFC4180_CSVReader
+        /// </summary>
+        /// <param name="csv">读取csv文件的TextReader</param>
+        public CSVReader(TextReader csv)
+        { this.csv = csv; }
+
+        /// <summary>
+        /// 从csv的字符串中构造IETF_RFC4180_CSVReader
+        /// </summary>
+        /// <param name="csv">csv字符串</param>
+        /// <param name="separator">分隔符</param>
+        public CSVReader(string csv)
+        { this.csv = new StringReader(csv); }
+
+        public void Dispose() => csv.Dispose();
+
+        /// <summary>
+        /// 从当前行弹出一个字段
+        /// </summary>
+        /// <returns>当前字段，若不能弹出字段则返回null</returns>
+        public string? PopField()
+        {
+            if (noMoreFields) return null;
+
+            bool isQuoted = csv.Peek() == '\"';
+            if (isQuoted) csv.Read();
+
+            while (true)
+            {
+                var peek = csv.Peek();
+
+                if (isQuoted)
+                {
+                    if (peek == -1)
+                        throw new InvalidDataException("字段双引号未闭合");
+
+                    if (peek == '\"')
+                    {
+                        csv.Read();
+                        var afterDoubleQuote = csv.Peek();
+
+                        if (afterDoubleQuote == Separator ||
+                            afterDoubleQuote == -1 ||
+                            afterDoubleQuote == '\r' ||
+                            afterDoubleQuote == '\n')
+                        {
+                            isQuoted = false;
+                            break;
+                        }
+                        else if (afterDoubleQuote == '\"')
+                            csv.Read();
+                        else
+                        {
+                            csv.Read();
+                            throw new InvalidDataException(
+                                $"在字段闭合后发现意外字符{(char)afterDoubleQuote}");
+                        }
+                    }
+                    else csv.Read();
+                }
+                else if (
+                    peek == Separator ||
+                    peek == '\n' ||
+                    peek == '\r' ||
+                    peek == -1)
+                    break;
+                else csv.Read();
+
+                fieldSb.Append((char)peek);
+            }
+
+            var field = fieldSb.ToString();
+            fieldSb.Clear();
+
+            var peek2 = csv.Peek();
+            if (peek2 == '\n' || peek2 == '\r' || peek2 == -1)
+                noMoreFields = true;
+            else if (peek2 == Separator)
+                csv.Read();
+            else throw new InvalidProgramException();
+
+            return field;
+        }
+
+        /// <summary>
+        /// 将光标移动到下一行
+        /// </summary>
+        /// <returns>如果文件结束，则返回false，否则返回true</returns>
+        public bool NextRecord()
+        {
+            while (!noMoreFields) PopField();
+
+            if (csv.Peek() == -1) return false;
+
+            if (csv.Peek() == '\r') csv.Read();
+            if (csv.Peek() == '\n') csv.Read();
+            if (csv.Peek() == -1) return false;
+            noMoreFields = false;
+            return true;
+        }
+
+        readonly TextReader csv;
+
+        readonly StringBuilder fieldSb = new();
+        bool noMoreFields = false;
+    }
+}
---
+++ IETF_RFC4180_CSVWriter/CSVWriter.cs
@@ -0,0 +1,85 @@
+﻿#nullable enable
+
+using System.Text;
+
+namespace IETF_RFC4180_CSV
+{
+    /// <summary>
+    /// CSV写入器
+    /// 其中CSV格式遵守IETF RFC 4180中所描述的格式，同时也允许修改分隔符以写入TSV和DSV
+    /// https://www.ietf.org/rfc/rfc4180.txt
+    /// </summary>
+    public class CSVWriter
+    {
+        /// <summary>
+        /// 写入csv文件时所使用的分隔符，修改此值以写入tsv、dsv等文件
+        /// 你需要自己保证每一条记录的字段数相同
+        /// </summary>
+        /// <value>分隔符</value>
+        public char Separator = ',';
+
+        /// <summary>
+        /// 是否强制每个字段均被双引号包裹
+        /// </summary>
+        public bool AlwaysWrap = false;
+
+        /// <summary>
+        /// 写入csv时所使用的换行符
+        /// </summary>
+        public string NewLine = System.Environment.NewLine;
+
+        /// <summary>
+        /// 在当前记录写入字段
+        /// </summary>
+        /// <param name="field">字段值</param>
+        public void WriteField(string field)
+        {
+            if (!isFirstFieldOfCurrentLine)
+                sb.Append(Separator);
+            isFirstFieldOfCurrentLine = false;
+
+            bool wrap =
+                AlwaysWrap ||
+                field.Contains(Separator) ||
+                field.Contains('\"') ||
+                field.Contains('\n') ||
+                field.Contains('\r') ||
+                field.Contains(',') ||
+                field.Contains('\t');
+
+            if (wrap) sb.Append('\"');
+            sb.Append(wrap ? field.Replace("\"", "\"\"") : field);
+            if (wrap) sb.Append('\"');
+        }
+
+        /// <summary>
+        /// 切换到下一条记录
+        /// </summary>
+        public void NextRecord()
+        {
+            isFirstFieldOfCurrentLine = true;
+            sb.Append(NewLine);
+        }
+
+        /// <summary>
+        /// 导出当前的csv
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 清空已经写入的csv
+        /// </summary>
+        public void Clear()
+        {
+            sb.Clear();
+            isFirstFieldOfCurrentLine = true;
+        }
+
+        readonly StringBuilder sb = new();
+        bool isFirstFieldOfCurrentLine = true;
+    }
+}
